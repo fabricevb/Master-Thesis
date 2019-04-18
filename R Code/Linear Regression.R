d35b450ea3672ace9f17e7121338884d2cd22ddb
Fabrice Van Boeckel <fabricevb@hotmail.com>
2019-04-18 13:55:05 +0200

    update

--- R Code/Linear Regression.R
+++ R Code/Linear Regression.R
@@ -31,7 +31,13 @@ model2 <- lm(GDP_year ~ GDP_year_lag1 + E_I, data = data)
 model3 <- lm(GDP_year ~ GDP_year_lag1 + E_I + Var_I, data = data)
 model4 <- lm(GDP_year ~ E_I + Var_I, data = data)
 
-stargazer(model1, model2, model3, model4, align = TRUE)
+stargazer(model1, model2, model3, model4, align = TRUE,
+          intercept.bottom = FALSE,
+          single.row = FALSE, 
+          df = FALSE,
+          covariate.labels = c("Constant","3 months lag of YoY GDP", "BSI", "Variance"),
+          dep.var.caption  = "Linear Regression",
+          dep.var.labels   = "Year on Year GDP (in \\%)")
 
 
 # plot different predictions
@@ -49,7 +55,6 @@ data$date <- as.Date(data$period)
 
 tmp <- data[c("date", "GDP_year_plot", "predicted_model1", "predicted_model2_plot", 
                 "predicted_model3_plot", "predicted_model4")]
-
 
 meltdf <- melt(tmp,id="date")
 meltdf$date <- as.Date(meltdf$date)
@@ -98,4 +103,54 @@ ggnostic(model3)
 ################################
 # Linear Autoregressive Models #
 ################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+######################### ALL INDICATORS
+
+# GDP YEAR  with difference previous month
+model1 <- lm(GDP_year ~ E_I + Var_I + Z_I + Var_Z_I, data = data)
+model2 <- lm(GDP_year ~ E_I + Var_I + Z_I, data = data)
+model3 <- lm(GDP_year ~ E_I + Z_I + Var_Z_I, data = data)
+
+stargazer(model1, model2, model3, align = TRUE)
+#, model2, model3, model4
+
+# plot different predictions
+data$predicted_model1 <- predict(model1, data)
+data$predicted_model2 <- predict(model2, data)
+data$predicted_model3 <- predict(model3, data)
+
+# plot the different models
+data$Obs = 1:nrow(data)
+data$GDP_year_plot <- with(data, interp1(Obs, GDP_year, Obs, "linear"))
+
+data$date <- as.Date(data$period)
+tmp <- data[c("date", "GDP_year_plot", "predicted_model1", "predicted_model2", "predicted_model3")]
+
+
+meltdf <- melt(tmp,id="date")
+meltdf$GDP_year <- data$GDP_year
+ggplot(meltdf,aes(x=date,y=value,colour=variable,group=variable)) + 
+  geom_line(na.rm=FALSE) + 
+  scale_x_date(date_breaks = "1 year", date_labels = "%Y")+
+  theme(axis.text.x = element_text(angle=45, hjust = 1)) +
+  geom_point(aes(y = GDP_year))
+
+
+ggnostic(model1)
 
--- R Code/tmp.R
+++ R Code/tmp.R
@@ -1,28 +1,26 @@
 
-Sys.setenv(JAVA_HOME="C:/Program Files/Java/jdk-11.0.2/")
-
-
-
 library(readxl)
 
-data <- read_excel("MI_2008-2018_T.xlsx", sheet = "MI022")
-
 library(rJava)
-install.packages("RJDemetra")
 
 library(RJDemetra)
 
 
-data <-read_excel("C:/Users/Fabrice/Documents/RS_975.xlsx",
-                   sheet="Sheet1")
-attach(indu)
-plot(Period, Industry)
+
+write.xlsx(recession, "GitHub/Master-Thesis/Datasets/recession.xlsx") 
+
+
+# plot
+data$GDP_year_plot <- with(data, interp1(Obs, GDP_year, Obs, "linear"))
+data$GDP_year_plot_lag <- Lag(data$GDP_year_plot, -1)
+
+recession <- subset(data, GDP_year_plot > GDP_year_plot_lag)
 
 
 
-
-
-autoplot(stl(MI027_E, s.window = 'periodic'), ts.colour = 'blue')
+ggplot(data) + 
+  geom_line(aes(x=period, y=GDP_year_plot)) +
+  geom_rect(data = recession, aes(xmin=min(period), xmax=max(period), ymin=-10, xmin=10))
 
 
 
