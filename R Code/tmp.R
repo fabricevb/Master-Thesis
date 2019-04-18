285b494dc889990966c5f6a50be9514689ce8468
Fabrice Van Boeckel <fabricevb@hotmail.com>
2019-04-18 13:56:13 +0200

    update

--- R Code/tmp.R
+++ R Code/tmp.R
@@ -17,11 +17,20 @@ data$GDP_year_plot_lag <- Lag(data$GDP_year_plot, -1)
 recession <- subset(data, GDP_year_plot > GDP_year_plot_lag)
 
 
+temp <- data.frame(
+  start = as.Date(c('2016-11-01', '2017-11-01')), 
+  end   = as.Date(c('2017-03-01', '2018-03-01')))
+
+dateRanges <- data.frame(
+  start = as.POSIXct(temp [,1], "%Y-%m-%d"),
+  end   = as.POSIXct(temp [,2], "%Y-%m-%d"))
+
+
 
 ggplot(data) + 
-  geom_line(aes(x=period, y=GDP_year_plot)) +
-  geom_rect(data = recession, aes(xmin=min(period), xmax=max(period), ymin=-10, xmin=10))
-
+  geom_rect(data = dateRanges, aes(xmin=start, xmax=end, ymin=-10, xmin=10))
+ 
+geom_line(aes(x=period, y=GDP_year_plot))
 
 
 
